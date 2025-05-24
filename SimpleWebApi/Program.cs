b05b5a20240dc0b85268efab6f24d81c7a459d26
Fernando Scavazzini Junior <ferfs_06@hotmail.com>
2025-05-24 18:41:45 -0300

    fix

--- SimpleWebApi/Program.cs
+++ SimpleWebApi/Program.cs
@@ -2,7 +2,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.WebHost.UseUrls("http://0.0.0.0:5000");
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
